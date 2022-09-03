54ea1d873f2d3148da5e3284e91a1e8c090acd30
ZAKİR00 <eliyevzakir946@gmail.com>
2022-09-03 15:22:42 -0700

    project

---
+++ src/test/java/az/myproject/goodreads/GoodreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.myproject.goodreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodreadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
